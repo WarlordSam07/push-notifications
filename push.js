f235a6eae6e3574f502ccdd873f7c6d6a0ac04c0
WarlordSam07 <pratik.lagaskar@mitaoe.ac.in>
2022-03-13 16:13:20 +0530

    initial6

--- push.js
+++ push.js
@@ -8,7 +8,7 @@ let vapidKeys = {
 push.setVapidDetails('mailto:test@code.ac.in', vapidKeys.publicKey, vapidKeys.privateKey)
 
 let sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/dPf4EjcN2Ew:APA91bH5vtHLbmCe17Ax6DhsCuLlMdm7r08v-bnb-bJc5iYjLQuGsDOSZKT9cotv8joO1r0r-LM65Uth79NKHkL8SLyc6MRJCrvZEGRtIepFgpOT_OSMrGwlDvXTYgZgEHq595Sv1FuE",
+    endpoint: "https://fcm.googleapis.com/fcm/send/dPf4EjcN2Ew:APA91bH5vtHLbmCe17Ax6DhsCuLlMdm7r08v-bnb-bJc5iYjLQuGsDOSZKT9cotv8joO1r0r-LM65Uth79NKHkL8SLyc6MRJCrvZEGRtIepFgpOT_OSMrGwlDvXTYgZgEHq595Sv1FuE",
     expirationTime: null,
     keys: {
         p256dh: "BARu_c1WwFwEhKI4ZNR2YX3mK0-8Xh8U_qZ7MFmY-YvC4J8Zjs5V0sjD5531ASC-SHw_QNpIonR8jYKZWBrmaZ8",
